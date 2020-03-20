0894b9ffbaeda4f97a79e16b1ed281835948e338
Ivan Uresti <iuresti@nearsoft.com>
2020-03-20 09:01:03 -0600

    Initial commit

---
+++ src/oop/inheritance/ingenico/IngenicoDisplay.java
@@ -0,0 +1,21 @@
+package oop.inheritance.ingenico;
+
+public class IngenicoDisplay {
+
+    /**
+     * Prints a message to specied position
+     *
+     * @param x       horizontal position
+     * @param y       vertical position
+     * @param message message to be printed
+     */
+    public void showMessage(int x, int y, String message) {
+    }
+
+    /**
+     * Clears the screen
+     */
+    public void clear() {
+
+    }
+}
---
+++ src/oop/inheritance/ingenico/IngenicoKeyboard.java
@@ -0,0 +1,11 @@
+package oop.inheritance.ingenico;
+
+public class IngenicoKeyboard {
+
+    /**
+     * @return key pressed
+     */
+    public String get() {
+        return "Key pressed";
+    }
+}
