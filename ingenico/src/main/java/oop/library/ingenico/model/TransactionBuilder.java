aa654f1e9cf239100109d1826ce59aab5b5edfe7
victorgs99 <vgomez_gs99@hotmail.com>
2024-05-11 14:43:34 -0600

    Add transaction builder

---
+++ ingenico/src/main/java/oop/library/ingenico/model/TransactionBuilder.java
@@ -0,0 +1,33 @@
+package oop.library.ingenico.model;
+
+import java.time.LocalDateTime;
+
+public class TransactionBuilder {
+    private int amountInCents;
+    private Card card;
+    private LocalDateTime localDateTime;
+
+    TransactionBuilder(){}
+
+    public TransactionBuilder amountInCents(int amountInCents){
+        this.amountInCents = amountInCents;
+
+        return this;
+    }
+
+    public TransactionBuilder card(Card card){
+        this.card = card;
+
+        return this;
+    }
+
+    public TransactionBuilder localDateTime(LocalDateTime localDateTime){
+        this.localDateTime = localDateTime;
+
+        return this;
+    }
+
+    public Transaction build(){
+        return new Transaction(amountInCents, card, localDateTime);
+    }
+}
